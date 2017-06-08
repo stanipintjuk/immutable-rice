1230710e0a074a594b951f13a014b48cb5883f54
Stani Pintjuk <stanislav.pintjuk@gmail.com>
2017-06-08 23:18:33 +0200

    Added example i3 configuration

--- configuration.nix
+++ configuration.nix
@@ -2,6 +2,6 @@
   # for autologin in wm
   username = "stani";
 
-  # for your own i3 keybindings and other preferences
+  # for your own i3 keybindings and other preferences (see i3key-example for an example)
   i3-keys-path = /home/stani/.config/i3/i3config_keys;
 }
